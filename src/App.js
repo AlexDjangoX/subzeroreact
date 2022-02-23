08c2b1267c7cba0efa7c557b81703f0562926744
AlexDjangoX <alexmonk17@gmail.com>
2022-02-23 12:32:08 +0100

    added Expenses and UI directory with css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
