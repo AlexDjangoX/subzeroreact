dc67f50b9afb21b50a5364850ede4f29228be71e
AlexDjangoX <alexmonk17@gmail.com>
2022-02-27 09:51:14 +0100

    hard-coding in ExpenseItem without props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 function App() {
+  const data = [
+    {
+      id: "e1",
+      title: "Helicopter",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Big Screen TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "New Car",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem data={data} />
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,21 @@
+import "../Expenses/css/ExpenseItem.css";
+
 import React from "react";
 
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  const expenseDate = new Date(2022, 3, 12);
+  const expenseTitle = "Helicopter";
+  const expenseAmount = 7654.23;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
