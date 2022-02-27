9f7ecea3276b88e4bb2834f2d04f28ff8261a7ea
AlexDjangoX <alexmonk17@gmail.com>
2022-02-27 12:36:40 +0100

    code looks correct in ExpenseItem.js but does not work. Introducing State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
   const clickHandler = () => {
-    console.log("Hello");
+    title = "new";
+    console.log("title");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change</button>
